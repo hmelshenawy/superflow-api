fadf74ea4bbda350240907a35689ddf4c8120662
Bondok <bondok@local>
2026-06-07 19:37:35 +0400

    Move operational modules under domain structure

---
+++ src/modules/technicians/technicians.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TechniciansController } from './technicians.controller';
+import { TechniciansService } from './technicians.service';
+import { PrismaModule } from '../../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [TechniciansController],
+  providers: [TechniciansService],
+  exports: [TechniciansService],
+})
+export class TechniciansModule {}
